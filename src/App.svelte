031c10ca97f1fb209458d123196ac6ee63de6bb1
hard-working-diamondcreator <lucas.lee.work61@gmail.com>
2021-05-14 23:28:08 -0500

    feat: 🎸 install prettier

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,33 @@
 <script lang="ts">
-	import './tailwind.svelte'
-	export let name: string;
+  import './tailwind.svelte'
+  export let name: string
+
+  let currentInputValue = ''
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p class="text-lg">Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div class="flex flex-col items-center p-6 max-w-xl mx-auto min-h-full">
+  <h1 class="text-4xl mb-6 text-center font-bold">Slugger the plain text</h1>
+
+  <div class="flex flex-col w-full space-y-6 mb-6">
+    <form class="flex flex-col w-full" spellcheck="false">
+      <label for="text" class="mb-2 font-bold">
+        Introduce a text to slug
+      </label>
+      <textarea
+        id="text"
+        name="text"
+        rows={3}
+        class="rounded-md text-dark p-4 focus:ring-4 ring-indigo-300 outline-none"
+        autocomplete="off"
+        bind:value={currentInputValue}
+        placeholder="Enter your text"
+      />
+    </form>
+  </div>
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  :global(body) {
+    @apply bg-dark text-white;
+  }
 </style>
